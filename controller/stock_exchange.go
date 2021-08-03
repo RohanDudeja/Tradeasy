adde881dfccf778a5c64306194b2b60188b81385
Rohan Dudeja <rohan.dudeja@razorpay.com>
2021-08-03 11:51:32 +0530

    1. added controller for exchange 2. moved exchange models to new dir

---
+++ controller/stock_exchange.go
@@ -0,0 +1,67 @@
+package controller
+
+import (
+	"Tradeasy/internal"
+	"Tradeasy/model/stock_exchange"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type StockExchangeActions interface {
+	ExecuteBuyOrder(c *gin.Context)
+	ExecuteSellOrder(c *gin.Context)
+	DeleteBuyOrder(c *gin.Context)
+	DeleteSellOrder(c *gin.Context)
+}
+type StockExchange struct {
+	Stocks   stock_exchange.Stocks
+	BuyBook  stock_exchange.BuyOrderBook
+	SellBook stock_exchange.SellOrderBook
+}
+
+func (se *StockExchange)ExecuteBuyOrder(c *gin.Context) {
+	var BuyOrder stock_exchange.BuyOrderBook
+	c.BindJSON(&BuyOrder)
+	err,msg := internal.ExecuteBuyOrder(se,&BuyOrder)
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK,[]byte(msg))
+	}
+}
+func (se *StockExchange)ExecuteSellOrder(c *gin.Context) {
+	var SellOrder stock_exchange.SellOrderBook
+	c.BindJSON(&SellOrder)
+	err,msg := internal.ExecuteSellOrder(se,&SellOrder)
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK,[]byte(msg))
+	}
+}
+func (se *StockExchange)DeleteBuyOrder(c *gin.Context) {
+	var BuyOrder stock_exchange.BuyOrderBook
+	c.BindJSON(&BuyOrder)
+	err,msg := internal.DeleteBuyOrder(se,&BuyOrder)
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK,[]byte(msg))
+	}
+
+}
+func (se *StockExchange)DeleteSellOrder(c *gin.Context) {
+	var SellOrder stock_exchange.SellOrderBook
+	c.BindJSON(&SellOrder)
+	err,msg := internal.DeleteSellOrder(se,&SellOrder)
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK,[]byte(msg))
+	}
+}
---
+++ model/stock_exchange/sell_order_book_model.go
@@ -0,0 +1,23 @@
+package stock_exchange
+
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
+// SellOrderBook ... schema for order book that stores sell orders
+type SellOrderBook struct {
+	gorm.Model
+	OrderID           string    `json:"order_id,omitempty" gorm:"primary_key; column:order_id"`
+	StockTickerSymbol string    `json:"stock_ticker_symbol,omitempty" gorm:"column:stock_ticker_symbol"`
+	OrderQuantity     int       `json:"order_quantity,omitempty" gorm:"column:order_quantity"`
+	OrderStatus       string    `json:"order_status,omitempty" gorm:"column:order_status"`
+	OrderPrice        int       `json:"order_price,omitempty" gorm:"column:order_price"`
+	CreatedAt         time.Time `json:"created_at" gorm:"column:created_at"`
+	UpdatedAt         time.Time `json:"updated_at" gorm:"column:updated_at"`
+	DeletedAt         time.Time `json:"deleted_at" gorm:"column:deleted_at"`
+}
+
+func (s *SellOrderBook) TableName() string{
+	return "sell_order_book"
+}
