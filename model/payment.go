bbe3ab5c494563e483c2e85ec9bd3abbafc1c46e
tarunguguloth <guguloth.tarun@razorpay.com>
2021-08-03 16:18:30 +0530

    formatted

--- model/payment.go
+++ model/payment.go
@@ -3,17 +3,16 @@ package Model
 import "github.com/golang/protobuf/ptypes/timestamp"
 
 type Payments struct {
-	id             string              `gorm:"primaryKey" json:"id"`
-	Userid         string              `json:"userid"`
-	User           Users               `gorm:"foreignKey:Userid" json:"user"`
-	razorpayUserId string              `gorm:"type:varchar" json:"razorpay_user_id"`
-	razorpayLinkId string              `gorm:"type:varchar" json:"razorpay_link_id"`
-	Amount         int                 `json:"amount"`
-	Type           string              `gorm:"type:varchar" json:"type"`
-	currentAmount  int                 `json:"current_amount"`
-	CreatedAt      timestamp.Timestamp `json:"created_at"`
-	UpdatedAt      timestamp.Timestamp `json:"updated_at"`
-	DeletedAt      timestamp.Timestamp `json:"deleted_at"`
+	id             string              `gorm:"primaryKey; column:id" json:"id"`
+	Userid         string              `gorm:"column:user_id" json:"user_id"`
+	razorpayUserId string              `gorm:"column:razorpay_user_id" json:"razorpay_user_id"`
+	razorpayLinkId string              `gorm:"column:razorpay_user_id" json:"razorpay_link_id"`
+	Amount         int                 `gorm:"column:amount" json:"amount"`
+	Type           string              `gorm:"column:type" json:"type"`
+	currentAmount  int                 `gorm:"column:current_amount" json:"current_amount"`
+	CreatedAt      timestamp.Timestamp `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt      timestamp.Timestamp `gorm:"column:updated_at" json:"updated_at"`
+	DeletedAt      timestamp.Timestamp `gorm:"column:deleted_at" json:"deleted_at"`
 }
 
 func (b *Payments) TableName() string {
