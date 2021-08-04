8097bc5c01d99fee13b9e758a8437908b1c2f37f
tarunguguloth <guguloth.tarun@razorpay.com>
2021-08-04 21:28:22 +0530

    AddedPaymentsControllerAndReqResBody

---
+++ internal/services/payments/payments.go
@@ -0,0 +1,11 @@
+package payments
+
+
+func AddAmount(addReq AddRequest, id string)(addRes AddResponse, err error){
+	return addRes,err
+}
+
+
+func WithdrawAmount(withdrawReq WithdrawRequest, id string )(withdrawRes WithdrawResponse, err error){
+	return withdrawRes,err
+}
---
+++ internal/services/payments/req_res_body.go
@@ -0,0 +1,25 @@
+package payments
+
+type AddRequest struct {
+	Userid string `json:"user_id"`
+	Amount int    `json:"amount"`
+}
+type AddResponse struct {
+	Userid         string `json:"user_id"`
+	Amount         int    `json:"amount"`
+	Type           string `json:"type"`
+	CurrentBalance int    `json:"current_balance"`
+	Message        string `json:"message"`
+}
+
+type WithdrawRequest struct {
+	Userid string `json:"user_id"`
+	Amount int    `json:"amount"`
+}
+type WithdrawResponse struct {
+	Userid         string `json:"user_id"`
+	Amount         int    `json:"amount"`
+	Type           string `json:"type"`
+	CurrentBalance int    `json:"current_balance"`
+	Message        string `json:"message"`
+}
