8a1322e89000f012e275b60f1feea003a41c114d
Rohan Dudeja <rohan.dudeja@razorpay.com>
2021-08-04 13:30:18 +0530

    added default values and removed foreign key reference

--- migration/20210803123705_stocks.sql
+++ migration/20210803123705_stocks.sql
@@ -2,16 +2,16 @@
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS stocks(
     id int not null AUTO_INCREMENT primary key,
-    stock_name varchar(255),
-    open_price int,
-    stock_ticker_symbol varchar(255),
-    ltp int,
-    high_price int,
-    low_price int,
-    previous_day_close int,
-    percentage_change int,
-    created_at timestamp,
-    updated_at timestamp,
+    stock_name varchar(255) not null,
+    open_price int not null,
+    stock_ticker_symbol varchar(255) not null,
+    ltp int not null,
+    high_price int not null,
+    low_price int not null,
+    previous_day_close int not null,
+    percentage_change int not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
     deleted_at timestamp
 );
 -- +goose StatementEnd
--- migration/20210803131854_sell_order_book.sql
+++ migration/20210803131854_sell_order_book.sql
@@ -3,13 +3,12 @@
 CREATE TABLE IF NOT EXISTS sell_order_book(
     id varchar(255) not null AUTO_INCREMENT primary key,
     order_id varchar(255) not null,
-    foreign key (order_id) references pending_orders(order_id),
-    stock_ticker_symbol varchar(255),
-    order_quantity int,
-    order_status varchar(255),
-    order_price int,
-    created_at timestamp,
-    updated_at timestamp,
+    stock_ticker_symbol varchar(255) not null,
+    order_quantity int not null,
+    order_status varchar(255) default 'pending',
+    order_price int not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp,
     deleted_at timestamp
 );
 -- +goose StatementEnd
