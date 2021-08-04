3f11e90a0c1cd9d6a19e245db41a3fcb01c1f5e3
Rohan Dudeja <rohan.dudeja@razorpay.com>
2021-08-04 12:50:24 +0530

    Added foreign key reference

--- migration/20210803131854_sell_order_book.sql
+++ migration/20210803131854_sell_order_book.sql
@@ -1,8 +1,9 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS sell_order_book(
-    id int not null unique AUTO_INCREMENT primary key,
+    id varchar(255) not null unique AUTO_INCREMENT primary key,
     order_id varchar(255) not null,
+    foreign key (order_id) references pending_orders(order_id),
     stock_ticker_symbol varchar(255),
     order_quantity int,
     order_status varchar(255),
