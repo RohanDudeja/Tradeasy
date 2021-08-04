b45bca30d409e28e79788d7757b9dbebf45dbad5
Ayush Mittal <ayush.m@razorpay.com>
2021-08-04 12:12:19 +0530

    Changed json tag

--- internal/model/watchlist.go
+++ internal/model/watchlist.go
@@ -5,8 +5,8 @@ import (
 )
 
 type Watchlist struct {
-	Id        int       `json:"watchlist_id,omitempty" gorm:"column:id;primary_key"`
-	Name      string    `gorm:"column:name" json:"watchlist_name,omitempty"`
+	Id        int       `json:"id,omitempty" gorm:"column:id;primary_key"`
+	Name      string    `gorm:"column:name" json:"name,omitempty"`
 	CreatedAt time.Time `json:"created_at" gorm:"column:created_at"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"column:updated_at"`
 	DeletedAt time.Time `json:"deleted_at" gorm:"column:deleted_at"`
