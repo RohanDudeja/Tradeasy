bb2c193ad8123e92954b18d388f90a3602af12a8
pkpraveen603 <praveen.k@razorpay.com>
2021-08-04 15:38:38 +0530

    Update routes.go for exchange engine api endpoints

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -14,6 +14,15 @@ func SetUpRouter() *gin.Engine  {
 		trade.POST(":Userid/sell",controller.SellOrder)
 		trade.PATCH(":OrderId/cancel",controller.CancelOrder)
 	}
+
+	exchange := r.Group("/order_book")
+	{
+		exchange.POST("buy_order", controller.ExecuteBuyOrder)
+		exchange.POST("sell_order", controller.ExecuteSellOrder)
+		exchange.DELETE("buy_order/:StockName", controller.DeleteBuyOrder)
+		exchange.DELETE("sell_order/:StockName", controller.DeleteSellOrder)
+		exchange.GET(":StockName/depth", controller.ViewMarketDepth)
+	}
 	 */
 	return r
 }
