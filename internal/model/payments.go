0e59dfe756de8db58c0b2d09eb8359a41027963f
tarunguguloth <guguloth.tarun@razorpay.com>
2021-08-04 10:49:19 +0530

    Changed currentAmount to currentBalance

--- internal/model/payments.go
+++ internal/model/payments.go
@@ -9,7 +9,7 @@ type Payments struct {
 	RazorpayLink   string    `gorm:"column:razorpay_link" json:"razorpay_link"`
 	Amount         int       `gorm:"column:amount" json:"amount"`
 	Type           string    `gorm:"column:type" json:"type"`
-	CurrentAmount  int       `gorm:"column:current_amount" json:"current_amount"`
+	CurrentBalance  int       `gorm:"column:current_amount" json:"current_amount"`
 	CreatedAt      time.Time `gorm:"column:created_at" json:"created_at"`
 	UpdatedAt      time.Time `gorm:"column:updated_at" json:"updated_at"`
 	DeletedAt      time.Time `gorm:"column:deleted_at" json:"deleted_at"`
