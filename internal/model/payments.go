a8e358233d0c3661292cecceaef2fb5c2acc4375
tarunguguloth <guguloth.tarun@razorpay.com>
2021-08-04 13:00:36 +0530

    minorchange

--- internal/model/payments.go
+++ internal/model/payments.go
@@ -3,12 +3,12 @@ package Model
 import "time"
 
 type Payments struct {
-	Id             int    `gorm:"primary_key; column:id" json:"id"`
+	Id             int       `gorm:"primary_key; column:id" json:"id"`
 	UserId         string    `gorm:"foreign_key; column:user_id" json:"user_id"`
 	RazorpayLinkId string    `gorm:"column:razorpay_link_id" json:"razorpay_link_id"`
 	RazorpayLink   string    `gorm:"column:razorpay_link" json:"razorpay_link"`
 	Amount         int       `gorm:"column:amount" json:"amount"`
-	Type           string    `gorm:"column:type" json:"type"`
+	PaymentType    string    `gorm:"column:payment_type" json:"payment_type"`
 	CurrentBalance int       `gorm:"column:current_balance" json:"current_balance"`
 	CreatedAt      time.Time `gorm:"column:created_at" json:"created_at"`
 	UpdatedAt      time.Time `gorm:"column:updated_at" json:"updated_at"`
