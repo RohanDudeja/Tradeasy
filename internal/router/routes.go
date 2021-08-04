c8518fa6306da61141dd4970957b8cee6d64c14f
Ayush Mittal <ayush.m@razorpay.com>
2021-08-04 17:10:50 +0530

    Committed user_management router

--- internal/router/routes.go
+++ internal/router/routes.go
@@ -1,12 +1,13 @@
 package router
 
 import (
+	"Tradeasy/internal/controller"
 	"github.com/gin-gonic/gin"
 )
 
 func SetUpRouter() *gin.Engine  {
 	r:=gin.Default()
-	/*
+
 	trade:=r.Group("/pending_orders")
 	{
 
@@ -14,6 +15,13 @@ func SetUpRouter() *gin.Engine  {
 		trade.POST(":Userid/sell",controller.SellOrder)
 		trade.PATCH(":OrderId/cancel",controller.CancelOrder)
 	}
-	 */
+	//users:=r.Group("/users")
+	//{
+	//	users.POST("/signup", controller.SignUp)
+	//	users.POST("/:Userid/details", controller.UserDetails)
+	//	users.POST("/signIn", controller.SignIn)
+	//	users.POST("/forgot", controller.ForgetPassword)
+	//	users.PATCH("/verify", controller.VerificationForPasswordChange)
+	//}
 	return r
 }
