65ed3d333b0822bbaf6e41b55a97ce26f4c0e0c2
tarunguguloth <guguloth.tarun@razorpay.com>
2021-08-04 11:31:46 +0530

    cahnged Userid to UserId

--- internal/model/payments.go
+++ internal/model/payments.go
@@ -4,7 +4,7 @@ import "time"
 
 type Payments struct {
 	Id             string    `gorm:"primary_key; column:id" json:"id"`
-	Userid         string    `gorm:"foreign_key; column:user_id" json:"user_id"`
+	UserId         string    `gorm:"foreign_key; column:user_id" json:"user_id"`
 	RazorpayLinkId string    `gorm:"column:razorpay_link_id" json:"razorpay_link_id"`
 	RazorpayLink   string    `gorm:"column:razorpay_link" json:"razorpay_link"`
 	Amount         int       `gorm:"column:amount" json:"amount"`
