2aeed66b0959c6969e2af4f9a23f1f69ca7eb679
pkpraveen603 <praveen.k@razorpay.com>
2021-08-04 11:39:29 +0530

    corrected models for exchange

--- internal/model/stock_exchange/buy_order_book.go
+++ internal/model/stock_exchange/buy_order_book.go
@@ -1,17 +1,20 @@
 package model
 
 import (
-	"github.com/jinzhu/gorm"
+	"time"
 )
 
 // BuyOrderBook ... schema for order book that stores buy orders
 type BuyOrderBook struct {
-	gorm.Model
-	OrderID           string    `json:"order_id,omitempty" gorm:"primary_key; column:order_id"`
+	ID                uint      `json:"id" gorm:"primary_key; column:id"`
+	OrderID           string    `json:"order_id,omitempty" gorm:"foreign_key:OrderId;column:order_id"`
 	StockTickerSymbol string    `json:"stock_ticker_symbol,omitempty" gorm:"column:stock_ticker_symbol"`
 	OrderQuantity     int       `json:"order_quantity,omitempty" gorm:"column:order_quantity"`
 	OrderStatus       string    `json:"order_status,omitempty" gorm:"column:order_status"`
 	OrderPrice        int       `json:"order_price,omitempty" gorm:"column:order_price"`
+	CreatedAt         time.Time `json:"created_at" gorm:"column:created_at"`
+	UpdatedAt         time.Time `json:"updated_at" gorm:"column:updated_at"`
+	DeletedAt         time.Time `json:"deleted_at" gorm:"column:deleted_at"`
 }
 
 func (b *BuyOrderBook) TableName() string{
