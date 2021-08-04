16b35c6519ec5d3092bd9633925bd9b831e02cb6
tarunguguloth <guguloth.tarun@razorpay.com>
2021-08-04 12:40:36 +0530

    made Id type to int

--- internal/model/payments.go
+++ internal/model/payments.go
@@ -3,13 +3,13 @@ package Model
 import "time"
 
 type Payments struct {
-	Id             string    `gorm:"primary_key; column:id" json:"id"`
+	Id             int    `gorm:"primary_key; column:id" json:"id"`
 	UserId         string    `gorm:"foreign_key; column:user_id" json:"user_id"`
 	RazorpayLinkId string    `gorm:"column:razorpay_link_id" json:"razorpay_link_id"`
 	RazorpayLink   string    `gorm:"column:razorpay_link" json:"razorpay_link"`
 	Amount         int       `gorm:"column:amount" json:"amount"`
 	Type           string    `gorm:"column:type" json:"type"`
-	CurrentBalance int       `gorm:"column:current_amount" json:"current_amount"`
+	CurrentBalance int       `gorm:"column:current_balance" json:"current_balance"`
 	CreatedAt      time.Time `gorm:"column:created_at" json:"created_at"`
 	UpdatedAt      time.Time `gorm:"column:updated_at" json:"updated_at"`
 	DeletedAt      time.Time `gorm:"column:deleted_at" json:"deleted_at"`
