b91c9cf71a7c29cfcec4c9b504fff59f9697b8ae
tarunguguloth <guguloth.tarun@razorpay.com>
2021-08-10 13:35:42 +0530

    minor changes

--- internal/services/payments/payments.go
+++ internal/services/payments/payments.go
@@ -3,6 +3,6 @@ package payments
 func AddAmount(addReq AddRequest, id string) (addRes AddResponse, err error) {
 	return addRes, err
 }
-func WithdrawAmount(withdrawReq WithdrawRequest, id string) (withdrawRes *WithdrawResponse, err error) {
+func WithdrawAmount(withdrawReq WithdrawRequest, id string) (withdrawRes WithdrawResponse, err error) {
 	return withdrawRes, err
 }
