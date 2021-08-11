f13f4b4f4a9667254a66dde07de046a520feb5e7
Ayush Mittal <40315346+Ayushnit@users.noreply.github.com>
2021-08-11 13:40:13 +0530

    Otp generation using redis (#36)
    * Commit
    
    * Commit
    
    * user management migrations committed
    
    * Watchlist migrations committed
    
    * Committed user_management router
    
    * Routers committed
    
    * user management controller
    
    * updated commit
    
    * Services user management
    
    * Committed otp generation part using redis
    
    * Made changes suggested today
    
    * commit
    
    * Commit
    
    * in cmd/migrate/main.go set value as ./migration in dir variable and also set your pswd in config.yaml file
    
    * Changes made
    
    * Testing of APIs done
    
    * resolved changes suggested
    
    * added redis in configuration
    
    * resolved changes
    
    * resolved conflicts
    
    * Resolved conflicts
    
    * changes
    
    * changes made
    
    * changes done
    
    * changes done
    
    * changes done
    
    * Added Auto increment in watchlist schema

---
+++ internal/provider/redis/redis.go
@@ -0,0 +1,44 @@
+package redis
+
+import (
+	"Tradeasy/config"
+	"github.com/go-redis/redis"
+	"log"
+	"time"
+)
+
+var redisClient *redis.Client
+
+func CreateClient() {
+	redisConfig := config.GetConfig().Redis
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     redisConfig.Host + ":" + string(redisConfig.Port),
+		Password: redisConfig.Password,
+		DB:       redisConfig.DB,
+	})
+	pong, err := TestClient()
+	if err != nil {
+		log.Fatalf("Redis: failed to create client: %v\n", err)
+	}
+	log.Println(pong)
+}
+func TestClient() (pong string, err error) {
+	pong, err = redisClient.Ping().Result()
+	return pong, err
+}
+
+func SetValue(key string, value string, expiry time.Duration) error {
+	err := redisClient.Set(key, value, expiry).Err()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func GetValue(key string) (string, error) {
+	value, err := redisClient.Get(key).Result()
+	if err != nil {
+		return "", err
+	}
+	return value, nil
+}
--- internal/router/routes.go
+++ internal/router/routes.go
@@ -8,6 +8,7 @@ import (
 
 func SetUpRouter() *gin.Engine {
 	r := gin.Default()
+
 	trade := r.Group("/pending_orders")
 	trade.Use(middleware.UserBasicAuth())
 	{
@@ -23,12 +24,14 @@ func SetUpRouter() *gin.Engine {
 		exchangeBuy.POST("buy_order", controller.ExecuteBuyOrder)
 		exchangeBuy.DELETE("buy_order/:order_id", controller.DeleteBuyOrder)
 	}
+
 	exchangeSell := r.Group("/sell_order_book")
 	exchangeSell.Use(middleware.ExchangeBasicAuth())
 	{
 		exchangeSell.POST("sell_order", controller.ExecuteSellOrder)
 		exchangeSell.DELETE("sell_order/:order_id", controller.DeleteSellOrder)
 	}
+
 	exchangeFetch := r.Group("/order_book")
 	exchangeFetch.Use(middleware.ExchangeBasicAuth())
 	{
@@ -41,6 +44,7 @@ func SetUpRouter() *gin.Engine {
 	//	websocket.GET("/stocks", webSocket.StockHandler)
 	//	websocket.GET("/orders", webSocket.OrderHandler)
 	//}
+
 	watchlist := r.Group("/user_watchlist")
 	watchlist.Use(middleware.UserBasicAuth())
 	{
@@ -48,6 +52,20 @@ func SetUpRouter() *gin.Engine {
 		watchlist.POST("/:watchlist_id/add", controller.AddStockEntry)
 		watchlist.DELETE("/:watchlist_id", controller.DeleteStockEntry)
 		watchlist.PATCH("/sort", controller.SortWatchlist)
+	}
+
+	userSignUp := r.Group("/users")
+	{
+		userSignUp.POST("/signup", controller.SignUp)
+	}
+
+	users := r.Group("/users")
+	users.Use(middleware.UserBasicAuth())
+	{
+		users.POST("/:user_id/details", controller.UserDetails)
+		users.POST("/sign_in", controller.SignIn)
+		users.POST("/forgot", controller.ForgetPassword)
+		users.PATCH("/verify", controller.VerificationForPasswordChange)
 	}
 	return r
 }
