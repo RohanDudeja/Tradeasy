ce9a9609b02fb993b52422ab15fd73e3cda9a211
Rohan Dudeja <rohan.dudeja@razorpay.com>
2021-08-11 16:54:10 +0530

    made changes

--- test/mysql/mock.go
+++ test/mysql/mock.go
@@ -6,12 +6,8 @@ import (
 	"log"
 )
 
-var db *sql.DB
-var mock sqlmock.Sqlmock
-
 func NewMock() (*sql.DB, sqlmock.Sqlmock) {
-	var err error
-	db, mock, err = sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+	db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
 	if err != nil {
 		log.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
 	}
