d1d9e5ee40b384c0ff8b14626f66028363d1393f
Ayush Mittal <ayush.m@razorpay.com>
2021-08-11 13:31:53 +0530

    Added Auto increment in watchlist schema

--- migration/20210811133020_watchlist.sql
+++ migration/20210811133020_watchlist.sql
@@ -1,12 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS watchlist(
-    id int not null primary key,
+    id int not null AUTO_INCREMENT primary key,
     name varchar(255) not null,
     created_at timestamp  not null,
     updated_at timestamp  not null,
     deleted_at timestamp default null
-);
+    );
 -- +goose StatementEnd
 
 -- +goose Down
