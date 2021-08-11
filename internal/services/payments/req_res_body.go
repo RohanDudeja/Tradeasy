87596606e8570c1239f65ac3c100564c491d6185
tarunguguloth <87662143+tarunguguloth@users.noreply.github.com>
2021-08-11 14:27:55 +0530

    Payment service (#46)
    * payments services added
    
    * payments service added
    
    * report services added
    
    * payments and reports routs added
    
    * minor changes
    
    * made suggested changes
    
    * minorchanges
    
    * yaml
    
    * added razorpay api
    
    * made changes
    
    * minor cahnges
    
    * minor changes
    
    * payment api service tested
    
    * minor changes
    
    * minor changes
    
    * resolved all changes

--- internal/services/payments/req_res_body.go
+++ internal/services/payments/req_res_body.go
@@ -4,11 +4,8 @@ type AddRequest struct {
 	Amount int64 `json:"amount"`
 }
 type AddResponse struct {
-	Userid         string `json:"user_id"`
-	Amount         int64  `json:"amount"`
-	Type           string `json:"type"`
-	CurrentBalance int64  `json:"current_balance"`
-	Message        string `json:"message"`
+	Message     string `json:"message"`
+	PaymentLink string `json:"payment_link"`
 }
 type WithdrawRequest struct {
 	Amount int64 `json:"amount"`
@@ -19,4 +16,50 @@ type WithdrawResponse struct {
 	Type           string `json:"type"`
 	CurrentBalance int64  `json:"current_balance"`
 	Message        string `json:"message"`
+}
+
+type RazorpayRequest struct {
+	Amount         int64  `json:"amount"`
+	Currency       string `json:"currency"`
+	AcceptPartial  bool   `json:"accept_partial"`
+	CallbackURL    string `json:"callback_url"`
+	CallbackMethod string `json:"callback_method"`
+}
+
+type RazorpayResponse struct {
+	AcceptPartial         bool          `json:"accept_partial"`
+	Amount                int64         `json:"amount"`
+	AmountPaid            int64         `json:"amount_paid"`
+	CallbackMethod        string        `json:"callback_method"`
+	CallbackURL           string        `json:"callback_url"`
+	CancelledAt           int           `json:"cancelled_at"`
+	CreatedAt             int           `json:"created_at"`
+	Currency              string        `json:"currency"`
+	Description           string        `json:"description"`
+	ExpireBy              int           `json:"expire_by"`
+	ExpiredAt             int           `json:"expired_at"`
+	FirstMinPartialAmount int           `json:"first_min_partial_amount"`
+	ID                    string        `json:"id"`
+	Payments              interface{}   `json:"payments"`
+	ReferenceID           string        `json:"reference_id"`
+	ReminderEnable        bool          `json:"reminder_enable"`
+	Reminders             []interface{} `json:"reminders"`
+	ShortURL              string        `json:"short_url"`
+	Status                string        `json:"status"`
+	UpdatedAt             int           `json:"updated_at"`
+	UpiLink               bool          `json:"upi_link"`
+	UserID                string        `json:"user_id"`
+}
+
+type CallbackParamRequest struct {
+	RazorpayPaymentID              string `json:"razorpay_payment_id" form:"razorpay_payment_id"`
+	RazorpayPaymentLinkID          string `json:"razorpay_payment_link_id" form:"razorpay_payment_link_id"`
+	RazorpayPaymentLinkReferenceID string `json:"razorpay_payment_link_reference_id" form:"razorpay_payment_link_reference_id"`
+	RazorpayPaymentLinkStatus      string `json:"razorpay_payment_link_status" form:"razorpay_payment_link_status"`
+	RazorpaySignature              string `json:"razorpay_signature" form:"razorpay_signature"`
+}
+
+type CallbackResponse struct {
+	Balance int64  `json:"balance"`
+	Status  string `json:"status"`
 }
